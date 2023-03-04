02a1d4d25de4e2b2cf746a6489b3d28cadd607d5
[Santheep Ganeswaran] <[gshantheep@gmail.com]>
2023-03-04 19:24:44 +0530

    Connecting to mongodb

--- flask_v1/application/__init__.py
+++ flask_v1/application/__init__.py
@@ -3,7 +3,7 @@ from flask_pymongo import PyMongo
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "64be0cc808f5ff47f9754b3a1d80eeef62314fa0"
-app.config["MONGO_URI"] = "mongodb://gshantheep:utmsaCdPlaOwcZdU@ac-bj5d9jb-shard-00-00.cz7xwta.mongodb.net:27017,ac-bj5d9jb-shard-00-01.cz7xwta.mongodb.net:27017,ac-bj5d9jb-shard-00-02.cz7xwta.mongodb.net:27017/?ssl=true&replicaSet=atlas-feqw05-shard-0&authSource=admin&retryWrites=true&w=majority"
+app.config["MONGO_URI"] = "mongodb+srv://gshantheep:cinadoc@cluster0.cz7xwta.mongodb.net/?retryWrites=true&w=majority"
 
 
 mongodb_client = PyMongo(app)

04065884a157c45db9f19587baefde9fa5bf8f78
[Santheep Ganeswaran] <[gshantheep@gmail.com]>
2023-03-06 19:26:51 +0530

    user signup added

--- flask_crud/app.py
+++ flask_crud/app.py
@@ -1,8 +1,8 @@
-from flask import Flask, jsonify,request
+from flask import Flask, jsonify,request, bcrypt
 from flask_pymongo import PyMongo
 from bson.json_util import dumps
 from bson.objectid import ObjectId
-from werkzeug.security import generate_password_hash,check_password_hash
+from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__) 
 app.secret_key = "cinadoc"
@@ -13,16 +13,17 @@ mongo = PyMongo(app)
 
 @app.route('/add',methods=['POST'])
 
-def add_user():
+def add_user():   
     _json = request.json
     _name = _json['name']
     _email = _json['email']
+    _phone = _json['phone']
     _password = _json['pwd']
 
-    if _name and _email and _password and request.method == 'POST':
+    if _name and _email and _password and _phone and request.method == 'POST':
         _hashed_password = generate_password_hash(_password)
-
-        id = mongo.db.user.insert_one({'name':_name, 'email':_email, 'pwd':_hashed_password})
+        
+        id = mongo.db.user.insert_one({'name':_name, 'email':_email, 'phone':_phone, 'pwd':_hashed_password})
 
         resp = jsonify("User created")
 
@@ -44,8 +45,54 @@ def not_found(error=None):
     resp.status_code = 404
     return resp
 
+@app.route('/users')
+def users():
+    users = mongo.db.user.find()
+    resp = dumps(users)
+    return resp
+
+@app.route('/user/<id>')
+def user(id):
+    user = mongo.db.user.find_one({'_id':ObjectId(id)})
+    resp = dumps(user)
+    return resp
+
+@app.route('/delete/<id>', methods=['DELETE'])
+def delete_user(id):
+    mongo.db.user.delete_one({'_id':ObjectId(id)})
+    resp = jsonify("User deleted")
+    resp.status_code = 200
+    return resp
+
+@app.route('/update/<id>', methods=['PUT'])
+def update_user(id):
+    _id = id
+    _json = request.json
+    _name = _json['name']
+    _email = _json['email']
+    _phone = _json['phone']
+    _password = _json['pwd']
+
+    if _name and _email and _password and _phone and request.method == 'PUT':
+        _hashed_password = generate_password_hash(_password)
+    
+        mongo.db.user.update_one({'_id': ObjectId(['$olid']) if '$olid' in _id else ObjectId(_id) }, {'$set': {'name':_name, 'email': _email,'pwd': _hashed_password}})
+        resp = jsonify('User updated')
+        resp.status_code = 200
+        return resp
+    else:
+        return not_found()
+
+
+
+    
 if __name__ == "__main__":
     app.run(debug=True)
+
+
+
+
+
 
 """
 from flask import Flask

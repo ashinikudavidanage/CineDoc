dc0ae9f7776d93e2cd89e27f3028d87489e1c0c6
[Santheep Ganeswaran] <[gshantheep@gmail.com]>
2023-03-20 19:06:17 +0530

    model integrated with backend

--- flask_crud/app.py
+++ flask_crud/app.py
@@ -1,13 +1,23 @@
+from __future__ import division, print_function
+# coding=utf-8
+import sys
+import os
+import glob
+import re
+
 from flask import Flask, jsonify,request, redirect, url_for, render_template
 from flask_pymongo import PyMongo
 from bson.json_util import dumps
 from bson.objectid import ObjectId
 from werkzeug.security import generate_password_hash, check_password_hash
+from werkzeug.utils import secure_filename
 
 import numpy as np 
+from keras.applications import VGG19
 from keras.applications.imagenet_utils import preprocess_input, decode_predictions
 from keras.models import load_model
-from keras.preprocessing import image
+import keras.utils as image
+#from keras.preprocessing import image
 
 app = Flask(__name__) 
 app.secret_key = "cinadoc"
@@ -26,14 +36,10 @@ def add_user():
     _password = _json['pwd']
 
     if _name and _email and _password and _phone and request.method == 'POST':
-        _hashed_password = generate_password_hash(_password)
-        
+        _hashed_password = generate_password_hash(_password)   
         id = mongo.db.user.insert_one({'name':_name, 'email':_email, 'phone':_phone, 'pwd':_hashed_password})
-
         resp = jsonify("User created")
-
         resp.status_code = 200
-
         return resp
 
     else:
@@ -100,6 +106,45 @@ def login():
         return "Incorrect password"
 
 
+
+model = load_model('acc73.h5')
+model.make_predict_function()
+
+def model_predict(img_path, model):
+    img = image.load_img(img_path, target_size=(255,255))
+    x = image.img_to_array(img)
+    x = np.expand_dims(img, axis=0)
+    x = preprocess_input(x)
+    preds = model.predict(x)
+    return preds
+
+@app.route('/',methods=['GET'])
+def index():
+    return render_template('index.html')
+
+
+@app.route('/predict',methods=['GET','POST'])
+def upload():
+    if request.method == 'POST':
+        f = request.files['file']
+    
+        basepath = os.path.dirname(__file__)
+        file_path = os.path.join(
+            basepath, 'uploads', secure_filename(f.filename))
+        f.save(file_path)
+
+       
+        preds = model_predict(file_path, model)
+
+        predicted_label = np.argmax(preds)
+
+        if predicted_label == 0:
+            result = "Rough Bark"
+            return result
+        else:
+            result = "Stripe canker"
+            return result
+                    
 
 if __name__ == "__main__":
     app.run(debug=True)

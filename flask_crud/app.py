a01aeef7936063b54fe42551671806d11244b2af
Ganeswaran Santheep <86246570+SantheepG@users.noreply.github.com>
2023-04-06 10:07:07 +0530

    Final commit

--- flask_crud/app.py
+++ flask_crud/app.py
@@ -1,22 +1,14 @@
 # Importing necessary libraries
+import os
 from flask import Flask, jsonify, request
 from flask_pymongo import PyMongo
-from bson.json_util import dumps
-from bson.objectid import ObjectId
-from werkzeug.security import generate_password_hash
 import bcrypt
-from io import BytesIO
-import requests
 import numpy as np 
 from keras.applications.imagenet_utils import preprocess_input
 from keras.models import load_model
 import keras.utils as image
 from pymongo import MongoClient
-import dropbox
-
-# Dropbox setup
-dbx = dropbox.Dropbox('sl.Bbt7SavKopMJjFqe3LEt2wTwPhBzjP1cci3LX1_ihEXQDCuomRHg2JfyekMOHn-cCSJqZK8atawi5elp76mFQfHVzNm6QIVLedWJjaJKUKisyI2ieZJolknmoI0wP9fl2O7cl9_LFs3m')
-
+from werkzeug.utils import secure_filename
 
 # Setting up mongodb
 app = Flask(__name__) 
@@ -29,51 +21,69 @@ mongo = PyMongo(app)
 
 # User registeration
 @app.route('/register',methods=['POST'])
-
 def register_user():   
-    _json = request.json
-    _name = _json['name']
-    _email = _json['email']
-    _phone = _json['phone']
-    _encrypted_pswd = bcrypt.hashpw(_json['pwd'].encode('utf-8'), bcrypt.gensalt()) # Pswd encryption
+    # Extracting values from dictionary
+    data = request.json
+    username = data['name']
+    email = data['email']
+    phone = data['phone']
+    # Pswd encryption
+    encrypted_pswd = bcrypt.hashpw(data['pwd'].encode('utf-8'), bcrypt.gensalt()) 
+    # Existing username check
+    existing_user = collection.find_one({'name': username})
 
-    existing_user = collection.find_one({'name': _name})
-
-    if _name and _email and _encrypted_pswd and _phone and request.method == 'POST':
-
-        if existing_user:
-            response = jsonify("Username already taken")
-            response.status_code = 404
-            return response
+    try:
+        if username and email and encrypted_pswd and phone and request.method == 'POST':
+            if existing_user:
+                response = jsonify("Username already taken")
+                response.status_code = 400
+                return response
+            
+            else:
+                id = collection.insert_one({'name':username, 'email':email, 'phone':phone, 'pwd':encrypted_pswd })
+                response = jsonify("User created successfully!")
+                response.status_code = 200
+                return response                 
+            
         else:
-            id = collection.insert_one({'name':_name, 'email':_email, 'phone':_phone, 'pwd':_encrypted_pswd })
-            response = jsonify("User created successfully!")
-            response.status_code = 200
-            return response   
+            response = jsonify("Required fields are empty")
+            response.status_code = 400
+            return response             
+ 
+    except:
+        response = jsonify("Something went wrong. Please try again")
+        response.status_code = 400
+        return response    
 
-    else:
-        resp  = jsonify("Required fields are empty")
-        resp.status_code = 404
-        return resp    
-
+    
 # User login
-@app.route('/login')
+@app.route('/login', methods=['POST'])
 def login():
-
-    _json = request.json
-    username = _json['name']
-    password = _json['pwd']
+    data = request.json
+    username = data['name']
+    password = data['pwd']
 
     if username and password :
-        user = collection.find_one({'name':username})
-        if bcrypt.checkpw(password.encode('utf-8'), user['pwd']):
-            return jsonify("Login success")
-        else:
-            return jsonify("Incorrect username or password")
-        
+        try:
+            user = collection.find_one({'name':username})
+            # decrypting password
+            if bcrypt.checkpw(password.encode('utf-8'), user['pwd']):
+                response = jsonify("Login success")
+                response.status_code = 200
+                return response 
+
+            else:
+                response = jsonify("Incorrect username or password")
+                response.status_code = 400
+                return response 
+        except:
+            response = jsonify("Incorrect username or password")
+            response.status_code = 400
+            return response 
     else:
-        return jsonify("Required fields are empty")
-    
+        response = jsonify("Required fields are empty")
+        response.status_code = 400
+        return response 
 
 # Prediction
 model = load_model('acc73.h5')
@@ -87,38 +97,35 @@ def model_predict(img_path, model):
     preds = model.predict(x)
     return preds
 
-# Uploading img to dropbox
-def upload_to_dropbox(file_stream, file_name):
-    dbx.files_upload(file_stream.read(), '/' + file_name)
-    shared_link_metadata = dbx.sharing_create_shared_link('/' + file_name)
-    shared_link_url = shared_link_metadata.url
-    return shared_link_url.replace('?dl=0', '?dl=1')
-
-# Img upload & classification    
 @app.route('/predict',methods=['GET','POST'])
-def predict():
+def upload():
+    try:
+        # Getting img from form data  & saving inside upload folder
+        f = request.files['image']
+        basepath = os.path.dirname(__file__)
+        file_path = os.path.join(basepath, 'uploads', secure_filename(f.filename))
+        f.save(file_path)
 
-    if request.method == 'POST':
-
-        file = request.files['image']
-        file_stream = file.stream
-        file_name = file.filename
-        # URL of the image
-        shared_link = upload_to_dropbox(file_stream, file_name)
-
-        # Get the content of the URL using requests
-        response = requests.get(shared_link)  
-        preds = model_predict(BytesIO(response.content), model)
+        # passing the img to model_predict()
+        preds = model_predict(file_path, model)
         predicted_label = np.argmax(preds)
-        if predicted_label == 0:
-            return jsonify("Rough Bark")
-        else:
-            return jsonify("Stripe canker")
-            
-          
 
+        if predicted_label == 0:
+            response = jsonify("Rough Bark")
+            response.status_code = 200
+            return response 
+        
+        else:
+            response = jsonify("Stripe canker")
+            response.status_code = 200
+            return response 
+    except:
+        response = jsonify("Image upload unsuccessful. Please try again")
+        response.status_code = 400
+        return response 
+                    
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug = True)
 
 
 
